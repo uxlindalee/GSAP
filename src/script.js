292b0845a1abe00137b58976aef9b357506cb040
Eunji Linda Lee <74166415+uxlindalee@users.noreply.github.com>
2022-11-14 22:25:31 +0900

    update from home
    havn't solve the scroll yet

--- src/script.js
+++ src/script.js
@@ -26,7 +26,7 @@ gltfLoader.setDRACOLoader(dracoLoader);
 let model;
 let mixer;
 // let modelClone;
-let model1, model2, model3, model4, model5, model6, model7, model8, model9, model10;
+// let model1, model2, model3, model4, model5, model6, model7, model8, model9, model10;
 const mixers = [];
 
 gltfLoader.load(
@@ -40,60 +40,67 @@ gltfLoader.load(
 				object.geometry.computeVertexNormals();
 			}
 		});
+		// mixer = new THREE.AnimationMixer(model);
+		// mixer.clipAction(gltf.animations[0]).play();
+
 		mixer = new THREE.AnimationMixer(model);
-		mixer.clipAction(gltf.animations[0]).play();
+		const action = mixer.clipAction(gltf.animations[0]);
+		createAnimation(mixer, action, gltf.animations[0]);
+		action.play();
+
 		model.position.set(0, 0, 0);
-		model.scale.set(0.07, 0.07, 0.07);
+		model.scale.set(0.7, 0.7, 0.7);
 		model.castShadow = true;
 		model.receiveShadow = true;
-		// scene.add(model);
+		scene.add(model);
 
-		model1 = clone(model);
-		model2 = clone(model);
-		model3 = clone(model);
-		model4 = clone(model);
-		model5 = clone(model);
-		model6 = clone(model);
-		model7 = clone(model);
-		model8 = clone(model);
-		model9 = clone(model);
-		model10 = clone(model);
+		// model1 = clone(model);
+		// model2 = clone(model);
+		// model3 = clone(model);
+		// model4 = clone(model);
+		// model5 = clone(model);
+		// model6 = clone(model);
+		// model7 = clone(model);
+		// model8 = clone(model);
+		// model9 = clone(model);
+		// model10 = clone(model);
 
-		model1.position.set(0, 0, 0);
-		model2.position.set(-2, 0, 0.5);
-		model3.position.set(2, 0, 0.5);
-		model4.position.set(-1, 0, 0.7);
-		model5.position.set(-2.5, 0, -0.5);
-		model6.position.set(5, 0, 0.5);
-		model7.position.set(7, 0, 1);
-		model8.position.set(-6, 0, 3);
-		model9.position.set(5, 0, 0.5);
-		model10.position.set(5, 0, 0.9);
+		// model1.position.set(0, 0, 0);
+		// model2.position.set(-2, 0, 0.5);
+		// model3.position.set(2, 0, 0.5);
+		// model4.position.set(-1, 0, 0.7);
+		// model5.position.set(-2.5, 0, -0.5);
+		// model6.position.set(5, 0, 0.5);
+		// model7.position.set(7, 0, 1);
+		// model8.position.set(-6, 0, 3);
+		// model9.position.set(5, 0, 0.5);
+		// model10.position.set(5, 0, 0.9);
 
-		const mixer1 = new THREE.AnimationMixer(model1);
-		const mixer2 = new THREE.AnimationMixer(model2);
-		const mixer3 = new THREE.AnimationMixer(model3);
-		const mixer4 = new THREE.AnimationMixer(model4);
-		const mixer5 = new THREE.AnimationMixer(model5);
-		const mixer6 = new THREE.AnimationMixer(model6);
-		const mixer7 = new THREE.AnimationMixer(model7);
-		const mixer8 = new THREE.AnimationMixer(model8);
-		const mixer9 = new THREE.AnimationMixer(model9);
-		const mixer10 = new THREE.AnimationMixer(model10);
+		// const mixer1 = new THREE.AnimationMixer(model1);
+		// const mixer2 = new THREE.AnimationMixer(model2);
+		// const mixer3 = new THREE.AnimationMixer(model3);
+		// const mixer4 = new THREE.AnimationMixer(model4);
+		// const mixer5 = new THREE.AnimationMixer(model5);
+		// const mixer6 = new THREE.AnimationMixer(model6);
+		// const mixer7 = new THREE.AnimationMixer(model7);
+		// const mixer8 = new THREE.AnimationMixer(model8);
+		// const mixer9 = new THREE.AnimationMixer(model9);
+		// const mixer10 = new THREE.AnimationMixer(model10);
 
-		mixer1.clipAction(gltf.animations[0]).play();
-		mixer2.clipAction(gltf.animations[0]).play();
-		mixer3.clipAction(gltf.animations[0]).play();
-		mixer4.clipAction(gltf.animations[0]).play();
-		mixer5.clipAction(gltf.animations[0]).play();
-		mixer6.clipAction(gltf.animations[0]).play();
-		mixer7.clipAction(gltf.animations[0]).play();
-		mixer8.clipAction(gltf.animations[0]).play();
-		mixer9.clipAction(gltf.animations[0]).play();
-		mixer10.clipAction(gltf.animations[0]).play();
+		// mixer1.clipAction(gltf.animations[0]).play();
+		// mixer2.clipAction(gltf.animations[0]).play();
+		// mixer3.clipAction(gltf.animations[0]).play();
+		// mixer4.clipAction(gltf.animations[0]).play();
+		// mixer5.clipAction(gltf.animations[0]).play();
+		// mixer6.clipAction(gltf.animations[0]).play();
+		// mixer7.clipAction(gltf.animations[0]).play();
+		// mixer8.clipAction(gltf.animations[0]).play();
+		// mixer9.clipAction(gltf.animations[0]).play();
+		// mixer10.clipAction(gltf.animations[0]).play();
 
-		scene.add(model1, model2, model3, model4, model5, model6, model7, model8, model9, model10);
-		mixers.push(mixer, mixer1, mixer2, mixer3, mixer4, mixer5, mixer6, mixer7, mixer8, mixer9, mixer10);
+		// scene.add(model1, model2, model3, model4, model5, model6, model7, model8, model9, model10);
+		// mixers.push(mixer, mixer1, mixer2, mixer3, mixer4, mixer5, mixer6, mixer7, mixer8, mixer9, mixer10);
+		// scene.add(model)
 	},
 
 	progress => {
@@ -190,88 +197,68 @@ let previousTime = 0;
 /**
  * gsap
  */
+gsap.registerPlugin(ScrollTrigger);
 const $wrap = document.getElementById("wrap");
 const $canvasSection = $wrap.querySelector(".section-intro");
-// window.addEventListener("scroll", () => {
-// 	scrollY = canvasSection.scrollY;
-// 	console.log(scrollY);
-// });
 
-// gsap.fromTo(
-// 	canvas.scene,
-// 	{ scale: 50, fill: "#2db400" },
-// 	{
-// 		scale: 0.5,
-// 		fill: "#ffffff",
-// 		ease: "expo.easeOut",
-// 		scrollTrigger: {
-// 			trigger: canvasSection,
-// 			start: "top top",
-// 			end: "bottom bottom",
-// 			scrub: true,
-// 		},
-// 	}
-// );
+function createAnimation(mixer, action, clip) {
 
-// Animation
-// function createAnimation(mixer1, action) {
-// 	let proxy = {
-// 		get time() {
-// 			return mixer1.time;
-// 		},
+    let proxy = {
 
-// 		set time(value) {
-// 			action.paused = false;
+        get time() {
 
-// 			mixer1.setTime(value);
+            return mixer.time;
 
-// 			action.paused = true;
-// 		},
-// 	};
+        },
 
-// 	let scrollingTL = gsap
-// 		.timeline({
-// 			scrollTrigger: {
-// 				trigger: $canvasSection,
+        set time(value) {
 
-// 				start: "top top",
+            action.paused = false;
 
-// 				end: "+=500%",
+            mixer.setTime(value);
 
-// 				pin: true,
+            action.paused = true;
 
-// 				scrub: true,
+        }
 
-// 				onUpdate: function () {},
-// 			},
-// 		})
+    };
 
-// 		.to(proxy, {
-// 			time: position.x * 0.01,
-// 		});
-// }
+    let scrollingTL = gsap.timeline({
 
-// let scrollY = window.scrollY;
-// let currentSection = 0;
+        scrollTrigger: {
 
-// window.addEventListener("scroll", () => {
-// 	scrollY = window.scrollY;
+            trigger: $canvasSection,
 
-// 	const newSection = Math.round(scrollY / sizes.height);
+            start: "top top",
 
-// 	if (newSection != currentSection) {
-// 		currentSection = newSection;
+            end: "+=500%",
 
-// 		// console.log(sectionMeshes)
-// 		gsap.to(sectionMeshes[currentSection].rotation, {
-// 			duration: 1.5,
-// 			ease: "power2.inOut",
-// 			x: "+=6",
-// 			y: "+=3",
-// 			z: "+=1.5",
-// 		});
-// 	}
-// });
+            pin: true,
+
+            scrub: true,
+
+            onUpdate: function () {
+
+            }
+
+        }
+
+    })
+
+        .to(proxy, {
+
+            time: clip.duration,
+
+        })
+
+}
+
+
+
+
+
+
+
 
 const tick = () => {
 	const elapsedTime = clock.getElapsedTime();
@@ -279,19 +266,22 @@ const tick = () => {
 	previousTime = elapsedTime;
 
 	//Update mixer
-	for (const mixer of mixers) mixer.update(deltaTime);
+	// for (const mixer of mixers) mixer.update(deltaTime);
+	if (mixer) mixer.update(deltaTime);
+
 
 	if (model) {
-		model1.position.x += 0.005;
-		model2.position.x += 0.003;
-		model3.position.x -= 0.007;
-		model4.position.x += 0.009;
-		model5.position.x -= 0.002;
-		model6.position.x += 0.004;
-		model7.position.x -= 0.007;
-		model8.position.x += 0.009;
-		model9.position.x -= 0.002;
-		model10.position.x += 0.007;
+		// model.position.x += 0.001;
+		// model1.position.x += 0.005;
+		// model2.position.x += 0.003;
+		// model3.position.x -= 0.007;
+		// model4.position.x += 0.009;
+		// model5.position.x -= 0.002;
+		// model6.position.x += 0.004;
+		// model7.position.x -= 0.007;
+		// model8.position.x += 0.009;
+		// model9.position.x -= 0.002;
+		// model10.position.x += 0.007;
 	}
 
 	// Render
@@ -302,16 +292,4 @@ const tick = () => {
 	// cancelAnimationFrame(requestID);
 };
 
-// tick();
-
-gsap.registerPlugin(ScrollTrigger);
-const $sectionIntro = $wrap.querySelector(".section-intro");
-
-console.log("is this here working?");
-ScrollTrigger.create({
-	trigger: $sectionIntro,
-	start: "top center",
-	end: "bottom bottom",
-	onEnter: () => tick(),
-	onLeaveBack: () => tick(),
-});
+tick();
